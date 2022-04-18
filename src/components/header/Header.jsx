946679a7c74eff3d5011d533d219dac752191360
PolushkinDenis <tigras666@mail.ru>
2022-04-18 19:59:45 +0400

    редактирование меню-бургера

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,23 +5,39 @@ import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
 import NavDropdown from 'react-bootstrap/NavDropdown'
 import './Header.css'
+import avatar from '../../images/avatar.png'
 
 const Header = () => {
     return (
         <Navbar expand="lg" bg="primary" variant="dark">
-        <Container>
-          <Navbar.Brand><Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <NavDropdown title="Меню" id="basic-nav-dropdown">
-                <NavDropdown.Item><Link to="/" className="links">Галерея</Link></NavDropdown.Item>
-                <NavDropdown.Item><Link to="/aboutMe" className="links">Обо мне</Link></NavDropdown.Item>
-                <NavDropdown.Divider />
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse></Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />         
-        </Container>
-      </Navbar>
+            <Container>
+                <Navbar.Brand><Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <NavDropdown title="Меню" id="basic-nav-dropdown">
+                            <NavDropdown.Item><Link to="/" className="links">Галерея</Link></NavDropdown.Item>
+                            <NavDropdown.Item><Link to="/aboutMe" className="links">Обо мне</Link></NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <div className="user-info">
+                                <img
+                                    src={avatar}
+                                    width="40"
+                                    height="40"
+                                    className="d-inline-block align-top"
+                                    alt="avatar"
+                                />
+                                <div>
+                                    Полушкин Денис
+                                </div>
+                            </div>
+                            <div className="user-email">
+                                tigraszver@gmail.com
+                            </div>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse></Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            </Container>
+        </Navbar>
     )
 }
 
