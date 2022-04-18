b98d2729d62f1154a3cc90197c3b96bde3aa5f95
PolushkinDenis <tigras666@mail.ru>
2022-04-18 17:20:11 +0400

    подробности картики и кнопка Назад

--- src/components/details/Details.jsx
+++ src/components/details/Details.jsx
@@ -1,16 +1,32 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 
-const Details = (props) => {
+const Details = () => {
+  const navigate = useNavigate();
   const location = useLocation();
-  console.log(props, " props");
-  console.log(location, " useLocation Hook");
   const data = location.state?.data;
+  
   console.log(data)
   return (
-    <div>
-      Details
-    </div>
+    <>
+      {data && (
+        <Card style={{ width: '20rem' }}>
+          <Card.Img variant="top" src={data.url} />
+          <Card.Body>
+            <Card.Title>Информация о картинке №{data.id}</Card.Title>
+            <Card.Text>
+              {data.title}
+            </Card.Text>
+            <Card.Text>
+              Ссылка на картинку: {data.url}
+            </Card.Text>
+            <Button variant="primary" onClick={() => navigate(-1)}>Назад</Button>
+          </Card.Body>
+        </Card>
+      )}
+    </>
   );
 }
 
